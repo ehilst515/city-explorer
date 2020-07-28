75454defc5f86d6a96c789b4fb3d83c9c5a90093
Enrique Hilst <enrique.hilst@gmail.com>
2020-07-28 12:26:44 -0500

    added location handler

--- server.js
+++ server.js
@@ -8,12 +8,21 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
+
 // Route Definitions
 app.get('/', rootHandler);
+app.get('/location', locationHandler)
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
 // Route Handlers
+function locationHandler(request, response) {
+  const city = 'seattle';
+  const locationData = require('./data/location.json');
+  const location = new Location(city, locationData);
+  response.status(200).send(location);
+}
+
 function rootHandler(request, response){
   response.status(200).send('City Explorer App')
 }
@@ -24,6 +33,14 @@ function notFoundHandler(request, response) {
 
 function errorHandler(error, request, response, next) {
   response.status(500).json({ error: true, message: error.message });
+}
+
+// Constructors
+function Location(city, locationData) {
+  this.search_query = city;
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = parseFloat(locationData[0].lat);
+  this.longitude = parseFloat(locationData[0].lon);
 }
 
 // App listener
